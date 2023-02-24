c09d46abdb09d36108060b2cdf9f7993fbb22cf5
Saihan786 <79385114+Saihan786@users.noreply.github.com>
2023-02-24 15:14:48 +0000

    FINISHED PRODUCT

--- Life/Simulator.java
+++ Life/Simulator.java
@@ -42,7 +42,7 @@ public class Simulator {
      */
     public static void main(String[] args) {
         Simulator sim = new Simulator();
-        sim.simulate(500);
+        sim.simulate(1);
 //        sim.simulate(30);
     }
 
@@ -96,7 +96,7 @@ public class Simulator {
     public void simulate(int numGenerations) {
         for (int gen = 1; gen <= numGenerations && view.isViable(field); gen++) {
             simOneGeneration();
-            delay(10);   // comment out to run simulation faster
+//            delay(10);   // comment out to run simulation faster
         }
     }
 
@@ -205,5 +205,13 @@ public class Simulator {
         catch (InterruptedException ie) {
             // wake up
         }
+    }
+
+    /**
+     * Returns the current generation as an integer.
+     * @return generation
+     */
+    public int getGeneration(){
+        return generation;
     }
 }
